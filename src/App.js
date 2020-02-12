134b9a160f29f09e6e59cef0204729ee60788ab4
wizardofqa <wizardofqa@outlook.com>
2020-02-12 01:43:04 -0800

    App component structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import uuid from 'uuid';
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
-function App() {
-  return (
-    <div>
+class App extends Component {
+  state = {
+    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    id: uuid()
+  }
+  render() {
+    console.log(this.state);
+    return (
       <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capiitalize text-center">todo input</h3>
+            <TodoInput />
+            <TodoList />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
